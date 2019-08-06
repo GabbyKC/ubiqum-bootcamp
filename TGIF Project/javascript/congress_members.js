72d426e3eec1c1e3be9c8193214a29909b88dba3
Gabriella-Kate <gabriellakate04@gmail.com>
2019-08-06 09:40:33 +0200

    state filtering and fetching live data

--- TGIF Project/javascript/congress_members.js
+++ TGIF Project/javascript/congress_members.js
@@ -1,8 +1,27 @@
-var members = data.results[0]["members"];
+var members = [];
 
-// generating members on page load :
-createTable(members);
-//
+function responseCallback() {
+  var data = JSON.parse(this.responseText);
+  members = data.results[0].members;
+
+  createTable(members);
+  createStateDropdown(members);
+}
+
+function errorCallback(err) {
+  console.log('oh snap, summit went wrong', err);
+}
+
+var urlParams = new URLSearchParams(window.location.search);
+var source = urlParams.get('source');
+
+var ajaxRequest = new XMLHttpRequest();
+ajaxRequest.onload = responseCallback;
+ajaxRequest.onerror = errorCallback;
+ajaxRequest.open('get', `https://api.propublica.org/congress/v1/116/${source}/members.json`, true);
+ajaxRequest.setRequestHeader('X-API-Key', 'a9w0MsdnBU28edFlltNLnVKJ4B14yol4Rwx1lO7i');
+ajaxRequest.setRequestHeader('Accept', 'application/json');
+ajaxRequest.send();
 
 // grabbing checkboxes for party filtering by ID:
 var democratCheckbox = document.getElementById("democrat");
@@ -20,20 +39,48 @@ function checkboxChange(e) {
     var isRepublicanChecked = republicanCheckbox.checked
     var isIndependentChecked = independentCheckbox.checked
 
+    var state = document.getElementById("stateDropdown").value;
+
     createTable(members, {
         "democrat": isDemocratChecked,
         "republican": isRepublicanChecked,
         "independent": isIndependentChecked,
+        "state": state
     });
 }
 
+// adding event listener to the State Dropdown :
+var stateDropdown = document.getElementById("stateDropdown");
+
+stateDropdown.addEventListener("change", function(event) {
+    var democratCheckbox = document.getElementById("democrat");
+    var republicanCheckbox = document.getElementById("republican");
+    var independentCheckbox = document.getElementById("independent");
+
+    var isDemocratChecked = democratCheckbox.checked
+    var isRepublicanChecked = republicanCheckbox.checked
+    var isIndependentChecked = independentCheckbox.checked
+
+    var selectedOption = event.target;
+    var state = selectedOption.value;
+
+    createTable(members, {
+        "democrat": isDemocratChecked,
+        "republican": isRepublicanChecked,
+        "independent": isIndependentChecked,
+        "state": state
+    });
+});
+
+
 // creating the actual table :
-// partyFilters is an object that is used to represent which parties to filter by :
-function createTable(members, partyFilters) {
-    var democratIsChecked = partyFilters ? partyFilters.democrat : false;
-    var republicanIsChecked = partyFilters ? partyFilters.republican : false;
-    var independentIsChecked = partyFilters ? partyFilters.independent : false;
-// if you dont pass partyFilters, it will default all checkboxes to false :
+// filters is an object that is used to represent which parties and state to filter by :
+function createTable(members, filters) {
+    var democratIsChecked = filters ? filters.democrat : false;
+    var republicanIsChecked = filters ? filters.republican : false;
+    var independentIsChecked = filters ? filters.independent : false;
+    var stateSelected = filters ? filters.state : "";
+// if you dont pass filters, it will default all checkboxes to false :
 
     var table = document.getElementById("senate-data");
 
@@ -48,21 +95,24 @@ function createTable(members, partyFilters) {
         var isRepublican = members[i]["party"] === "R";
         var isIndependent = members[i]["party"] === "I";
 
-// making sure that all members are displayed when no filters are checked :
-        if(!democratIsChecked && !republicanIsChecked && !independentIsChecked) {
-            var row = createRow(members[i]);
-            body.appendChild(row);
-        }
+        var state = members[i]["state"];
+        if (stateSelected === "" || state === stateSelected) {
+            // making sure that all members are displayed when no filters are checked :
+            if (!democratIsChecked && !republicanIsChecked && !independentIsChecked) {
+                var row = createRow(members[i]);
+                body.appendChild(row);
+            }
 
-        if (democratIsChecked && isDemocrat) {
-            var row = createRow(members[i]);
-            body.appendChild(row);
-        } else if (republicanIsChecked && isRepublican) {
-            var row = createRow(members[i]);
-            body.appendChild(row);
-        } else if (independentIsChecked && isIndependent) {
-            var row = createRow(members[i]);
-            body.appendChild(row);
+            if (democratIsChecked && isDemocrat) {
+                var row = createRow(members[i]);
+                body.appendChild(row);
+            } else if (republicanIsChecked && isRepublican) {
+                var row = createRow(members[i]);
+                body.appendChild(row);
+            } else if (independentIsChecked && isIndependent) {
+                var row = createRow(members[i]);
+                body.appendChild(row);
+            }
         }
     }
     table.appendChild(body);
@@ -102,4 +152,34 @@ function createCell(memberData) {
     cell.appendChild(cellText);
 
     return cell;
+}
+
+function createStateDropdown(members) {
+    var groupedStates = {};
+
+    for (var i = 0; i < members.length; i++) {
+        var key = members[i]["state"];
+        if (groupedStates[key]) {
+            groupedStates[key].push(members[i]);
+        } else {
+            groupedStates[key] = [members[i]];
+        }
+    }
+
+    var groupedStatesKeys = Object.keys(groupedStates);
+    groupedStatesKeys.sort();
+
+    var stateDropdown = document.getElementById("stateDropdown");
+    for (var i = 0; i < groupedStatesKeys.length; i++) {
+        var option = createOption(groupedStatesKeys[i]);
+        stateDropdown.appendChild(option);
+    }
+}
+
+function createOption(groupedStatesKeys){
+    var option = document.createElement("option");
+    var state = document.createTextNode(groupedStatesKeys);
+    option.appendChild(state);
+
+    return option;
 }
