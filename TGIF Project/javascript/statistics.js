e20e2592c8429156d627c1c3f96b9a0ece715f43
Gabriella-Kate <gabriellakate04@gmail.com>
2019-08-06 10:47:42 +0200

    fetching live data

--- TGIF Project/javascript/statistics.js
+++ TGIF Project/javascript/statistics.js
@@ -1,4 +1,24 @@
-var members = data.results[0]["members"];
+var members = [];
+
+function responseCallback() {
+  var data = JSON.parse(this.responseText);
+  members = data.results[0].members;
+}
+
+function errorCallback(err) {
+  console.log('oh snap, summit went wrong', err);
+}
+
+var urlParams = new URLSearchParams(window.location.search);
+var source = urlParams.get('source');
+
+var ajaxRequest = new XMLHttpRequest();
+ajaxRequest.onload = responseCallback;
+ajaxRequest.onerror = errorCallback;
+ajaxRequest.open('get', `https://api.propublica.org/congress/v1/116/${source}/members.json`, false);
+ajaxRequest.setRequestHeader('X-API-Key', 'a9w0MsdnBU28edFlltNLnVKJ4B14yol4Rwx1lO7i');
+ajaxRequest.setRequestHeader('Accept', 'application/json');
+ajaxRequest.send();
 
 var democratMembers = findDemocrats(members);
 var republicanMembers = findRepublicans(members);
