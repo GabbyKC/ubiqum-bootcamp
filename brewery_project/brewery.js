73a6c38764263b0f5df1c44d78deaead74283c0c
Gabriella-Kate <gabriellakate04@gmail.com>
2019-08-25 17:28:20 +0200

    infite scroll, fetching the rest of the pages

--- brewery_project/brewery.js
+++ brewery_project/brewery.js
@@ -1,15 +1,45 @@
-fetch('https://ubiqum-cors-anywhere.herokuapp.com/sandbox-api.brewerydb.com/v2/beers?key=1b66372cfda3abdd9b017e9fe2177901')
-    .then(response => {
-        return response.json();
-    })
-    .then(data => {
-        let beers = data.data;
-        createBeerGallery(beers);
-    })
-    .catch(err => {
-        console.log('oh snap, summit went wrong', err);
-    });
+var currentlyFetchingData = false;
+var page = 1;
 
+checkAndFetch();
+
+function checkAndFetch() {
+    if (haveReachedEndOfPage() && !currentlyFetchingData) {
+        fetchBeers(page);
+        page = page + 1;
+    }
+}
+
+$(window).scroll(function() { // Every time you scroll this will get called
+    // If we have reached the end of the HTML content AND we're not currently loading any data
+    checkAndFetch();
+});
+
+function fetchBeers(pageNumber) {
+    currentlyFetchingData = true;
+    $('#loader').removeClass('d-none');
+
+    console.log(`Will fetch page ${page}`);
+    fetch(`https://ubiqum-cors-anywhere.herokuapp.com/sandbox-api.brewerydb.com/v2/beers?key=1b66372cfda3abdd9b017e9fe2177901&p=${pageNumber}`)
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            let beers = data.data;
+
+            if (beers && beers.length > 0) {
+                createBeerGallery(beers);
+                $('#loader').addClass('d-none');
+                currentlyFetchingData = false;
+            } else {
+                $('#loader').addClass('d-none');
+                $('#end-of-content').removeClass('d-none');
+            }
+        })
+        .catch(err => {
+            console.log('oh snap, summit went wrong', err);
+        });
+}
 
 function createBeerGallery(beers) {
     let container = document.getElementById('beer-container');
@@ -31,17 +61,35 @@ function createBeerGallery(beers) {
         container.appendChild(flipContainer);
 
         let cardBack = document.createElement('div');
-        let description = document.createElement('p');
-        let descriptionText = document.createTextNode(getABVText(beers[i]));
+        let innerBeerName = document.createElement('p');
+        let innerBeernameText = document.createTextNode(beers[i].name);
+        let abvDetail = document.createElement('p');
+        let abvDetailText = document.createTextNode(getABVText(beers[i]));
+        let ibuDetail = document.createElement('p');
+        let ibuDetailText = document.createTextNode(getIBUText(beers[i]));
+        let organicDetail = document.createElement('p');
+        let organicDetailText = document.createTextNode(getOrganicDetail(beers[i]));
+        let statusDetail = document.createElement('p');
+        let statusDetailText = document.createTextNode(getStatus(beers[i]));
 
-        description.appendChild(descriptionText);
-        cardBack.appendChild(description);
+        innerBeerName.appendChild(innerBeernameText);
+        abvDetail.appendChild(abvDetailText);
+        ibuDetail.appendChild(ibuDetailText);
+        organicDetail.appendChild(organicDetailText);
+        statusDetail.appendChild(statusDetailText);
+        cardBack.appendChild(innerBeerName);
+        cardBack.appendChild(abvDetail);
+        cardBack.appendChild(ibuDetail);
+        cardBack.appendChild(organicDetail);
+        cardBack.appendChild(statusDetail);
         card.appendChild(cardBack);
 
         flipContainer.className = "flip-card";
+        flipContainer.setAttribute('data-id', beers[i].id);
         card.className = "flip-card-inner";
         cardFront.className = "flip-card-front";
         cardBack.className = "flip-card-back";
+        innerBeerName.className = "back-header"
     }
 }
 
@@ -57,4 +105,29 @@ function getABVText(beer) {
         return 'ABV: ' + beer.abv;
     }
     return 'ABV: -';
+}
+
+function getIBUText(beer) {
+    if (beer.hasOwnProperty('ibu')) {
+        return 'IBU: ' + beer.ibu;
+    }
+    return 'IBU: -';
+}
+
+function getOrganicDetail(beer) {
+    if (beer.hasOwnProperty('isOrganic')) {
+        return 'Organic: ' + beer.isOrganic;
+    }
+    return '';
+}
+
+function getStatus(beer) {
+    if (beer.hasOwnProperty('status')) {
+        return 'Status: ' + beer.status;
+    }
+    return '';
+}
+
+function haveReachedEndOfPage() {
+    return $(window).scrollTop() >= $(document).height() - $(window).height() - 10;
 }
